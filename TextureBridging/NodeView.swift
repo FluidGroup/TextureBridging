68f8b8e2e2934e1650a387044eca86c8365082a0
Muukii <muukii.app@gmail.com>
2019-05-19 15:23:02 +0900

    Add attributes to be unavailable methods UILabel has

--- TextureBridging/NodeView.swift
+++ TextureBridging/NodeView.swift
@@ -1,8 +1,123 @@
+//
+// NodeView
+//
+// Copyright (c) 2019 Hiroshi Kimura
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
 
 import UIKit
 import AsyncDisplayKit
 
+///
 open class NodeView<D: ASDisplayNode>: UILabel /* To use `textRect` method */ {
+  
+  // MARK: - Unavailable
+  
+  @available(*, unavailable)
+  override open var text: String? {
+    didSet {}
+  }
+  
+  @available(*, unavailable)
+  override open var font: UIFont! {
+    didSet {}
+  }
+  
+  @available(*, unavailable)
+  override open var textColor: UIColor! {
+    didSet {}
+  }
+  
+  @available(*, unavailable)
+  override open var shadowColor: UIColor? {
+    didSet {}
+  }
+  
+  @available(*, unavailable)
+  override open var shadowOffset: CGSize {
+    didSet {}
+  }
+  
+  @available(*, unavailable)
+  override open var textAlignment: NSTextAlignment {
+    didSet {}
+  }
+  
+  @available(*, unavailable)
+  override open var lineBreakMode: NSLineBreakMode {
+    didSet {}
+  }
+  
+  @available(*, unavailable)
+  open override var attributedText: NSAttributedString? {
+    didSet {}
+  }
+  
+  @available(*, unavailable)
+  open override var highlightedTextColor: UIColor? {
+    didSet {}
+  }
+  
+  @available(*, unavailable)
+  open override var isHighlighted: Bool {
+    didSet {}
+  }
+  
+  @available(*, unavailable)
+  open override var isEnabled: Bool {
+    didSet {}
+  }
+  
+  @available(*, unavailable)
+  open override var numberOfLines: Int {
+    didSet {}
+  }
+  
+  @available(*, unavailable)
+  open override var adjustsFontSizeToFitWidth: Bool {
+    didSet {}
+  }
+  
+  @available(*, unavailable)
+  open override var baselineAdjustment: UIBaselineAdjustment {
+    didSet {}
+  }
+  
+  @available(*, unavailable)
+  open override var minimumScaleFactor: CGFloat {
+    didSet {}
+  }
+  
+  @available(*, unavailable)
+  open override var allowsDefaultTighteningForTruncation: Bool {
+    didSet {}
+  }
+  
+  @available(*, unavailable)
+  open override func drawText(in rect: CGRect) {
+    super.drawText(in: rect)
+  }
+  
+  @available(*, unavailable)
+  open override var preferredMaxLayoutWidth: CGFloat {
+    didSet {}
+  }
   
   // MARK: - Properties
   
@@ -19,7 +134,7 @@ open class NodeView<D: ASDisplayNode>: UILabel /* To use `textRect` method */ {
     super.init(frame: frame)
     
     // To call `textRect(forBounds bounds: CGRect, limitedToNumberOfLines numberOfLines: Int)`
-    numberOfLines = 0
+    super.numberOfLines = 0
     isUserInteractionEnabled = true
     
     addSubnode(wrapperNode)
@@ -35,6 +150,7 @@ open class NodeView<D: ASDisplayNode>: UILabel /* To use `textRect` method */ {
     }
   }
   
+  @available(*, unavailable)
   public required init?(coder aDecoder: NSCoder) {
     fatalError("init(coder:) has not been implemented")
   }
