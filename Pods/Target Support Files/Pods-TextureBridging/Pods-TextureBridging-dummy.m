ed8d3ac84c3fda90832c793fdeb8972154bd15fe
Muukii <muukii.app@gmail.com>
2022-08-31 17:12:16 +0900

    Remove Pods commit

--- Pods/Target Support Files/Pods-TextureBridging/Pods-TextureBridging-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_TextureBridging : NSObject
-@end
-@implementation PodsDummy_Pods_TextureBridging
-@end
--- Pods/Target Support Files/TypedTextAttributes/TypedTextAttributes-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_TypedTextAttributes : NSObject
-@end
-@implementation PodsDummy_TypedTextAttributes
-@end
